f2753c6586da04137911933a9d459b9401f43339
Kweyuteshi <paul.kweyu@strathmore.edu>
2024-02-06 22:38:33 +0300

    Adding a few comments

--- dating-website/src/pages/LandingPage.tsx
+++ dating-website/src/pages/LandingPage.tsx
@@ -12,14 +12,20 @@ const LandingPage: React.FC = () => {
       <p className="text-center my-4 fs-2">Hoping you will find the sukuma to your ugali ;)</p>
 
       <div className="row">
+        {/*This maps the different profiles based on their Id numbers from the Json file*/}
         {profilesData.map((profile) => (
           <div key={profile.id} className="col-md-4 mb-4">
+
             <div className="card shadow">
+              {/*Display the profile photo and details of the specific profile */}
               <img src={profile.image} className="card-img-top" alt={profile.name} />
+
               <div className="card-body card-container">
+                {/*This contains the person's information*/}
                 <h5 className="card-title">{profile.name}</h5>
                 <p className="card-text">{`${profile.age} years old`}</p>
                 <p className="card-text"><i className="fas fa-location-pin"></i>{profile.location}</p>
+                {/*This button when clicked will take you to the specific profile page of that person based on their id number*/}
                 <Link to={`/profile/${profile.id}`} className="btn btn-primary">View Profile</Link>
               </div>
             </div>
